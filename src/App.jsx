b2c30e6652f4a78287a651c0013a7716b1b33787
Hans Lao <laohansxavier03@gmail.com>
2026-08-15 11:53:12 +0800

    Temporary rename to force Git tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './css/index.css'
 import './css/app.css'
 import { useState } from 'react';
-import Header from './components/Header';
+import Header from './components/HeaderTemp';
 import AboutModal from './components/AboutModal';
-import Body from './components/Body'
+import Body from './components/BodyTemp'
 
 function App() {
   // isAboutOpen has a truthy value of false
--- src/components/BodyTemp.jsx
+++ src/components/BodyTemp.jsx
@@ -1,6 +1,6 @@
 import '../css/index.css'
 import '../css/body.css'
-import Carousel from './Carousel'
+import Carousel from './CarouselTemp'
 
 function Body(){
 	return(
